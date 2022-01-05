793e8de3c6275a1bdcffc4dd9e9cd25c4311ee90
ADARSH RAJ <adarsh02raj@gmail.com>
2022-01-05 18:51:28 +0530

    v1.1

---
+++ let's_practice.py
@@ -0,0 +1,50 @@
+#Let's Practice --------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+# This program prints Hello, world!
+
+# print('Hello, world!')
+
+
+# chapter 1 - Introduction
+# ========================
+
+#---------- Practice Sheet -------------
+
+#---------- Question 1 -----------------
+ 
+# print('''Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.''')
+
+
+#---------- Question 2 -----------------
+'''On REPL table print
+5*1 <-press enter
+5*2 <-press enter
+again n again till 5*10
+'''
+
+#----------- Question 3 ----------------
+'''
+We use her playsound module
+as
+in terminal 
+1st) pip install playsound <-|
+2nd) 
+from playsound import playsound
+playsound ('play.mp3')
+ '''
+
+
+#---------------Question 3 ----------
+'''
+#install by pip
+import os
+print(os.listdir())
+'''
+
+
+# ---------- Question 4 -------------
+# labeling the above codes
