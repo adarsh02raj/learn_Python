ba21da9e6cd1eda0d0469ea110ff7ea1bea114a5
ADARSH RAJ <adarsh02raj@gmail.com>
2022-01-07 04:26:43 +0530

    v2.0

---
+++ chapter_02.py
@@ -0,0 +1,115 @@
+#---------------- Chapter 02 ------------------
+#----------------Variables & Data Types -------
+# a = "harry" or a = 'harry' or a = '''harry''' -- all are same.
+# b = 345
+# c = 45.32
+# print(a,b,c)
+
+#------------------printing the data type of variables-----------
+# a = 'harry'
+# b = 456
+# c = 45.56
+# d = True
+# e = None
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+
+# ---------------- Operators --------------
+
+# a = 3
+# b = 4
+# print("The value of a and b is", a+b)
+# print("The value of a and b is", a-b)
+# print("The value of a and b is", a*b)
+# print("The value of a and b is", a/b)
+# print("The value of a and b is", a%b)
+
+
+# --------------Assignment Operator ---------------
+# a= 34
+# a+= 34
+# a-= 34
+# a*= 34
+# a/=34
+# print(a)
+# 
+
+# ------------Comprasion Operator ----------
+
+# b = (a>c)
+# b = (a<c)
+# b = (a>=c)
+# b = (a<=c)
+# b = (a!=c)
+# b = (a==c)
+
+
+
+# ---------Logical Operator ----------------
+# 
+# bool1 = True
+# bool2 = False
+# print(bool1 or bool2)
+# print(bool1 and bool2)
+# print(not bool2)
+# 
+
+# ---------------Type Casting --------------------
+
+# a = "3434"
+# a = int(a)
+# print(a + 5)
+# print(type(a))
+
+
+# -------------------take input -----------------------
+# a  = input("Enter your name: ")
+# a = int(a) --------- Convert a into an integer(if possible)
+# print(a)
+# print(type(a))
+
+#  -------------------------------------------------------Chapter 02 _ Practice --------------------------------------
+
+
+# q1-------------------->
+# WAP to addtwo number
+# a = 3
+# b = 4
+# print("Sum = ",a+b)
+
+# q2 ------------------->
+# WAP to find remainder when a number is divided by 2.
+# a = input("Enter the number: ")
+# a = int(a)
+# print("Remainder is ",a%2)
+
+# q3 ------------------->
+# check the type of the variable assigned using input function.
+# a = input("Enter the name: ")
+# print(type(a))
+
+
+# Q4 ------------------>
+# a = input("Enter a number: ")
+# b = input("Enter another number: ")
+# a = int(a)
+# b = int(b)
+# print("Is a greater then b ? ",a>b)
+
+
+# q5 ------------------->
+# a = input("Enter a number: ")
+# b = input("Enter another number: ")
+# a = int(a)
+# b = int(b)
+# print("Average of entered nuber is: ",(a+b)/2)
+
+
+# q6 ------------------->
+a = input("Enter a number: ")
+a = int(a)
+print("Square of the given number: ",a*a)
