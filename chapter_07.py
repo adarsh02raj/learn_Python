c37f5ff3aa3ea7f4b9db635d84126f338c142773
ADARSH RAJ <adarsh02raj@gmail.com>
2022-01-14 00:59:42 +0530

    update v1.0

---
+++ chapter_07.py
@@ -0,0 +1,51 @@
+# loops in python ----------------->>>>>>>>>>>
+# quick quiz
+'''
+i = 1
+while(i<=50):
+    print(i)
+    i+=1
+'''
+'''WAP to print the content of a list using while loops'''
+'''
+mklist = [78, 45, "Harry", 456, 78, 74, 58, 985412, 1]
+i = 0
+while(i<len(mklist)):
+    print(mklist[i])
+    i+=1
+    '''
+
+'''
+fruits = ['Banana', 'Watermelon', 'Grapes', 'Mangoes']
+
+for item in fruits:
+    print(item)
+   '''
+
+# range in python
+'''print the range of 0 to 6'''
+'''
+for i in range(0, 7): 
+    print(i)
+for i in range(8):
+    print(i)
+'''
+# continue statement
+'''
+for i in range(4):
+    print("printing")
+    if(i == 2):
+        continue
+    print(i)
+'''
+# pass statement 
+'''
+l = [1,7,8]
+for item in l:
+    pass
+'''
+
+
+# practice sets --------------------->>>>>>>>>>>>>>>>>>
+# q1---------------->>>>>>>>
+
